7c6d2e444862f6bc21205b80dd68b5a44b70c73d
Hubert <hubert.osewski@contractors.roche.com>
2019-03-03 11:18:45 +0100

    update libraries

--- domain/src/main/kotlin/com/yossisegev/domain/common/TestTransformer.kt
+++ domain/src/main/kotlin/com/yossisegev/domain/common/TestTransformer.kt
@@ -6,6 +6,7 @@ import io.reactivex.*
  * Created by Yossi Segev on 13/11/2017.
  */
 class TestTransformer<T>: Transformer<T>() {
+
     override fun apply(upstream: Single<T>): SingleSource<T> {
         return upstream
     }
